1ffebc0799a44465542160e0bd084caedf688893
DimManolis <115085849+DimManolis@users.noreply.github.com>
2022-10-05 18:22:19 +0300

    Create NumberReverse.py

---
+++ NumberReverse.py
@@ -0,0 +1,12 @@
+## Have the function NumberReverse(str) take the str parameter being passed which will be a string of numbers, and return a new string with the numbers in reverse order.
+##Examples
+##Input: "1 2 3"
+##Output: 3 2 1
+#Input: "10 20 50"
+#Output: 50 20 10##
+  
+def NumberReverse(strParam):
+
+  return " ".join(reversed(strParam.split(" ")))
+# keep this function call here 
+print NumberReverse(raw_input())
